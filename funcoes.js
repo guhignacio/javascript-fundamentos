35e8d0937d16a876737df8d0896006728d69cfff
Guhignacio <lgcosta2004@gmail.com>
2026-02-12 17:43:26 -0300

    feat:Adiciona função nomesMaioresEmMaiusculo

--- funcoes.js
+++ funcoes.js
@@ -3,5 +3,6 @@ const filtrarMaiores =(pessoas) =>pessoas.filter(p=>p.idade>=18);
 const filtrarMenores =(pessoas) =>pessoas.filter (p=>p.idade<18);
 const nomesMaiusculos = (pessoas) =>pessoas.map(p => p.nome.toUpperCase());
 const contaMaiores = (pessoas) =>pessoas.filter(p => p.idade >= 18).length;
+const nomesMaioresEmMaiusculo = (pessoas) =>pessoas.filter(p => p.idade >=18).map(p => p.nome.toUpperCase());
 
-module.exports = { listarNomes, filtrarMaiores, filtrarMenores, nomesMaiusculos, contaMaiores };
+module.exports = { listarNomes, filtrarMaiores, filtrarMenores, nomesMaiusculos, contaMaiores,nomesMaioresEmMaiusculo };
--- index.js
+++ index.js
@@ -7,3 +7,4 @@ console.log("Maiores de 18:", filtrarMaiores(pessoas));
 console.log("Menores de 18:", filtrarMenores(pessoas));
 console.log("Nomes em maiúsculo:", nomesMaiusculos(pessoas));
 console.log("Quantidade de maiores de 18:", contaMaiores(pessoas));
+console.log("Nomes dos maiores de 18 em maiúsculo:", nomesMaioresEmMaiusculo(pessoas));
